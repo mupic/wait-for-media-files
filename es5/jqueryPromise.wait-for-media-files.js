17ebb6d252d8fffeb4258fe90ece7aa8e8fb9dc7
mupic <mupic@yandex.ru>
2019-09-08 05:10:27 +0400

    es5

---
+++ es5/jqueryPromise.wait-for-media-files.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _promise = _interopRequireDefault(require("./promise.wait-for-media-files"));
+
+jQuery(function ($) {
+  $.fn.prsWaitMediaFiles = function (options) {
+    return (0, _promise.default)(this, options);
+  };
+});
---
+++ es5/promise.wait-for-media-files.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+require("core-js/modules/es6.promise");
+
+require("core-js/modules/es6.object.to-string");
+
+var _waitForMediaFiles = _interopRequireDefault(require("./wait-for-media-files"));
+
+var promiseWaitMediaFiles = function promiseWaitMediaFiles(domElement, options) {
+  var callbacks = (0, _waitForMediaFiles.default)(domElement, options);
+  return {
+    all: function all() {
+      return new Promise(function (resolve) {
+        callbacks.all(resolve);
+      });
+    },
+    audio: function audio() {
+      return new Promise(function (resolve) {
+        callbacks.audio(resolve);
+      });
+    },
+    iframe: function iframe() {
+      return new Promise(function (resolve) {
+        callbacks.iframe(resolve);
+      });
+    },
+    image: function image() {
+      return new Promise(function (resolve) {
+        callbacks.image(resolve);
+      });
+    },
+    video: function video() {
+      return new Promise(function (resolve) {
+        callbacks.video(resolve);
+      });
+    }
+  };
+};
+
+var _default = promiseWaitMediaFiles;
+exports.default = _default;
